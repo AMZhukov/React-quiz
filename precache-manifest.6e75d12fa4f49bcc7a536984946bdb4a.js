449b904714aa4c8f6e621ae15f6c37b01ee093cc
Andrey Zhukov <zhukov290791@gmail.com>
2020-02-26 21:11:55 +0500

    Updates

---
+++ precache-manifest.6e75d12fa4f49bcc7a536984946bdb4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35848ae5efe780101b8bad12646c2aba",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "e92ba9e32f269654eaab",
+    "url": "/react-quiz/static/css/main.84aed471.chunk.css"
+  },
+  {
+    "revision": "43e8fa360bfeadedf2ab",
+    "url": "/react-quiz/static/js/2.7f974023.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-quiz/static/js/2.7f974023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e92ba9e32f269654eaab",
+    "url": "/react-quiz/static/js/main.674762f0.chunk.js"
+  },
+  {
+    "revision": "393653e383602ffadf78",
+    "url": "/react-quiz/static/js/runtime-main.4c7d2a6c.js"
+  }
+]);
